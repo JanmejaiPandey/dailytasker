ac338cfa0390571638bb748b5b7642bd28c61478
JanmejaiPandey <48506722+JanmejaiPandey@users.noreply.github.com>
2019-05-28 16:09:45 +0530

    office ui added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from "react"
+import { PrimaryButton, TextField, DefaultButton, DetailsList } from "office-ui-fabric-react"
+import { Fabric } from 'office-ui-fabric-react/lib/Fabric';
 import './App.css';
 
 function App() {
@@ -38,30 +40,67 @@ function App() {
     const refreshPage = () => {
         window.location.reload();
     }
+    const _columns = [
+        { key: 'column1', name: 'id', fieldName: 'id', minWidth: 100, maxWidth: 200, isResizable: true },
+        { key: 'column2', name: 'Task', fieldName: 'task', minWidth: 100, maxWidth: 200, isResizable: true },
+        { key: 'column3', name: 'Description', fieldName: 'description', minWidth: 100, maxWidth: 200, isResizable: true }
+      ];
     return (
-        <div>
-            {
-                tasks.map((t) =>
-                    (
-                        <div key={t.id}>
-                            {t.task}
-                        </div>
-                    )
-                )
-            }
+        <fabric>
             <form onSubmit={handleSubmit}>
-                <label>
-                    Task:
-        <input
-                        type="text"
-                        value={task}
-                        onChange={e => setTask(e.target.value)}
-                    />
-                </label>
-                <input type="submit" value="Submit" />
+                <TextField
+                    label="Task:"
+                    underlined
+                    required
+                    placeholder="Enter task here"
+                    value={task}
+                    onChange={e => setTask(e.target.value)}
+                />
+                <DefaultButton
+                    type="submit"
+                    text="Submit"
+                />
+                <PrimaryButton
+                    data-automation-id="test"
+                    text="Refresh"
+                    onClick={refreshPage}
+                    allowDisabledFocus={true}
+                />
             </form>
-            <button onClick={refreshPage}>Refresh</button>
-        </div>
+            {/* <table>
+                {
+                    tasks.map((t) =>
+                        (
+                            <tr key={t.id}>
+                                <td>
+                                    {t.task}
+                                </td>
+                            </tr>
+                        )
+                    )
+                }
+            </table> */}
+            {/* <div className={exampleChildClass}>{selectionDetails}</div> */}
+            {/* <TextField
+                //   className={exampleChildClass}
+                label="Filter by name:"
+                //   onChange={this._onFilter}
+                styles={{ root: { maxWidth: '300px' } }}
+            /> */}
+            {/* <MarqueeSelection selection={this._selection}> */}
+            <DetailsList
+                items={tasks}
+                columns={_columns}
+                setKey="set"
+                // layoutMode={DetailsListLayoutMode.fixedColumns}
+                // selection={this._selection}
+                selectionPreservedOnEmptyClick={true}
+                ariaLabelForSelectionColumn="Toggle selection"
+                ariaLabelForSelectAllCheckbox="Toggle selection for all items"
+            //onItemInvoked={this._onItemInvoked}
+            />
+            {/* </MarqueeSelection> */}
+        </fabric>
     );
 }
 
